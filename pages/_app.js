1ab958af30e74d0825299821ed86c72b8cdfd10c
nikolidakisfotios <nikolidakisgroup@gmail.com>
2026-01-12 15:44:59 +0200

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,24 @@
-import './globals.css'
-
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <style jsx global>{`
+        @import url('https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400;0,700;1,400&display=swap');
+        
+        html, body {
+          padding: 0;
+          margin: 0;
+          background-color: #0A0A0A;
+          color: #F3F4F6;
+          font-family: 'Playfair Display', serif;
+        }
+
+        * {
+          box-sizing: border-box;
+        }
+      `}</style>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
